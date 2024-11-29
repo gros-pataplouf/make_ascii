0de6ee57fa7fd21c027deb7c2551b0d95bdbc671
gros-pataplouf <sceres@pm.me>
2024-11-29 17:12:04 +0100

    read star image

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-pass
+from PIL import Image
+
+with Image.open("xmas.png") as im:
+    px = im.load()
+    height = im.size[0]
+    width = im.size[1]
+    for i in range(0, height, 3):
+        for j in range(0, width):
+            print("*" if px[i,j] == (0, 0, 0, 255) else " ", end="")
+        print("\r")
+
